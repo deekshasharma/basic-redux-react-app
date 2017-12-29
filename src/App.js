bbfea39cd3da9ab5ce295768e05c305ae7d8187c
Deeksha <deeksha@Deekshas-MacBook-Pro.local>
2017-12-29 16:14:19 +1300

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import './App.css';
+import {connect} from 'react-redux';
+import * as courseActions from './actions/courseActions'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            course: {title: ''}
+        }
+    }
+
+    onTitleChange = (event) => {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course})
+    };
+
+    onClickSave = () => {
+        this.props.dispatch(courseActions.createCourse(this.state.course))
+    };
+
+    courseRow = (course, index) =>{
+        return <div key={index}>{course.title}</div>
+    };
+
+    render() {
+        debugger;
+        return (
+            <div>
+                <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
+                <h2>Add Course</h2>
+                <input
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title}
+                />
+
+                <input
+                    type="submit"
+                    onClick={this.onClickSave}
+                    value="Save"
+                />
+            </div>
+
+        );
+    }
+}
+
+function mapStateToProps (state, ownProps) {
+    debugger;
+    return{
+        courses: state.courses
+    }
+};
+
+function mapDispatchToProps() {
+        // If we don't implement this, then connect will attach dispatch function automatically
+};
+
+export default connect(mapStateToProps)(App);
+
+//Alternative to above line is:
+// const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps) => This returns a function
+// export default connectedStateAndProps(App);

5d1feabb2e19b206bbdb9ad8bdb6de20bfefe171
Deeksha <deeksha@Deekshas-MacBook-Pro.local>
2017-12-30 15:22:07 +1300

    Aysnchronous calls to server with redux-thunk

--- src/App.js
+++ src/App.js
@@ -3,46 +3,48 @@ import './App.css';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from './actions/courseActions'
+import CourseList from './course/CourseList';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: {title: ''}
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+        // this.state = {
+        //     course: {title: ''}
+        // }
+    // }
 
-    onTitleChange = (event) => {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course})
-    };
+    // onTitleChange = (event) => {
+    //     const course = this.state.course;
+    //     course.title = event.target.value;
+    //     this.setState({course: course})
+    // };
 
-    onClickSave = () => {
-        this.props.actions.createCourse(this.state.course);
-    };
+    // onClickSave = () => {
+    //     this.props.actions.createCourse(this.state.course);
+    // };
 
     courseRow = (course, index) => {
         return <div key={index}>{course.title}</div>
     };
 
     render() {
+        const {courses} = this.props;
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title}
-                />
+                <CourseList courses={courses}/>
+                {/*<h2>Add Course</h2>*/}
+                {/*<input*/}
+                    {/*type="text"*/}
+                    {/*onChange={this.onTitleChange}*/}
+                    {/*value={this.state.course.title}*/}
+                {/*/>*/}
 
-                <input
-                    type="submit"
-                    onClick={this.onClickSave}
-                    value="Save"
-                />
+                {/*<input*/}
+                    {/*type="submit"*/}
+                    {/*onClick={this.onClickSave}*/}
+                    {/*value="Save"*/}
+                {/*/>*/}
             </div>
 
         );
